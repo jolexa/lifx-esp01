cbda625a3fd6fd4dae6494f00a5e1d79e0846e02
Jeremy Olexa <jolexa@jolexa.net>
2019-01-22 18:41:15 -0600

    Revision 2 of API GW, includes API KEY

--- apigw.tf
+++ apigw.tf
@@ -3,27 +3,11 @@ resource "aws_api_gateway_rest_api" "endpoint" {
   description = "API for lifx-proxy lambda function"
 }
 
-resource "aws_api_gateway_resource" "proxy" {
-  rest_api_id = "${aws_api_gateway_rest_api.endpoint.id}"
-  parent_id   = "${aws_api_gateway_rest_api.endpoint.root_resource_id}"
-  path_part   = "{proxy+}"
-}
-
-resource "aws_api_gateway_method" "proxy" {
+resource "aws_api_gateway_stage" "lifx" {
+  stage_name    = "lifx"
   rest_api_id   = "${aws_api_gateway_rest_api.endpoint.id}"
-  resource_id   = "${aws_api_gateway_resource.proxy.id}"
-  http_method   = "POST"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "lambda" {
-  rest_api_id = "${aws_api_gateway_rest_api.endpoint.id}"
-  resource_id = "${aws_api_gateway_method.proxy.resource_id}"
-  http_method = "${aws_api_gateway_method.proxy.http_method}"
-
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = "${aws_lambda_function.lifx.invoke_arn}"
+  deployment_id = "${aws_api_gateway_deployment.endpoint.id}"
+  tags = "${var.tags}"
 }
 
 resource "aws_api_gateway_method" "proxy_root" {
@@ -31,6 +15,7 @@ resource "aws_api_gateway_method" "proxy_root" {
   resource_id   = "${aws_api_gateway_rest_api.endpoint.root_resource_id}"
   http_method   = "POST"
   authorization = "NONE"
+  api_key_required = "true"
 }
 
 resource "aws_api_gateway_integration" "lambda_root" {
@@ -45,12 +30,11 @@ resource "aws_api_gateway_integration" "lambda_root" {
 
 resource "aws_api_gateway_deployment" "endpoint" {
   depends_on = [
-    "aws_api_gateway_integration.lambda",
-    "aws_api_gateway_integration.lambda_root",
+    "aws_api_gateway_integration.lambda_root"
   ]
 
   rest_api_id = "${aws_api_gateway_rest_api.endpoint.id}"
-  stage_name  = "test"
+  stage_name  = "lifx"
 }
 
 resource "aws_lambda_permission" "apigw" {
@@ -64,3 +48,25 @@ resource "aws_lambda_permission" "apigw" {
   source_arn = "${aws_api_gateway_deployment.endpoint.execution_arn}/POST/*"
 }
 
+# Usage plan is required for API key
+resource "aws_api_gateway_usage_plan" "lifx-usage-plan" {
+  name = "lifx-usage-plan"
+  api_stages {
+    api_id = "${aws_api_gateway_rest_api.endpoint.id}"
+    stage  = "${aws_api_gateway_deployment.endpoint.stage_name}"
+  }
+throttle_settings {
+    burst_limit = 5
+    rate_limit  = 10
+  }
+}
+
+resource "aws_api_gateway_api_key" "lifx-api-key" {
+  name = "lifx-api-key"
+}
+
+resource "aws_api_gateway_usage_plan_key" "main" {
+  key_id        = "${aws_api_gateway_api_key.lifx-api-key.id}"
+  key_type      = "API_KEY"
+  usage_plan_id = "${aws_api_gateway_usage_plan.lifx-usage-plan.id}"
+}
